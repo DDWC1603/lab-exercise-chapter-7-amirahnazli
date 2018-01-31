8c343f17a254aed31a835f1c93061bbf4a9cc361
princeamirah <33883963+amirahnazli@users.noreply.github.com>
2018-01-31 22:46:40 +0800

    Task Complete

--- Exercise-Enum.cpp
+++ Exercise-Enum.cpp
@@ -1,11 +1,20 @@
+//nur amirah afiqah binti mohd.nazli
+//a17dw2172
 
+#include<iostream>
+using namespace std;
 
+enum minggu {ahad, senin, selasa, rabu, khamis, jumaat, sabtu};
+
+int main()
+{
 //if the day is sunday, what day is it?
 //write your code using enum and Sunday is your default 'day'
 
 minggu hariIni;
+hariIni = rabu;
 
-cout<<"Hari ke-"hariIni+1;
+cout<<"Hari ke-" <<hariIni+1;
+
 return 0;
-
-
+}
